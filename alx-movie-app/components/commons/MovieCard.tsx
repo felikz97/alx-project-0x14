c4ffeaad5b3dbb1851ead8d41674b0c3015e5560
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-21 12:25:29 +0300

    1. Bootstrap MovieApp

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieCard: React.FC = () => {
+  return (
+    <div className="movie-card">
+      MovieCard
+    </div>
+  );
+};
+
+export default MovieCard;
---
+++ alx-movie-app/pages/movies/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoviesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+    </div>
+  );
+};
+
+export default MoviesPage;
